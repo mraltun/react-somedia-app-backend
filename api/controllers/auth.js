b735f922da5bbed02b8be315bc9c2e13a918793e
Murat Altun <mraltun@gmail.com>
2022-10-30 13:32:25 +0300

    Logout functionality in auth controller

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -59,4 +59,12 @@ export const login = (req, res) => {
   });
 };
 
-export const logout = (req, res) => {};
+export const logout = (req, res) => {
+  res
+    .clearCookie("accessToken", {
+      secure: true,
+      sameSite: "none",
+    })
+    .status(200)
+    .json("User has been logged out");
+};
