0c2d0e9789fe8e405a0c777265c9d77392f8e90f
Murat Altun <mraltun@gmail.com>
2022-10-31 12:53:44 +0300

    Added addPost functionality

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import moment from "moment";
 import { db } from "../connect.js";
 
 export const getPosts = (req, res) => {
@@ -14,6 +15,30 @@ export const getPosts = (req, res) => {
       if (err) return res.status(500).json(err);
 
       return res.status(200).json(data);
+    });
+  });
+};
+
+export const addPost = (req, res) => {
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not logged in!");
+
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
+
+    const q =
+      "INSERT INTO posts(`desc`, `img`, `createdAt`, `userId`) VALUES (?)";
+    const values = [
+      req.body.desc,
+      req.body.img,
+      moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+      userInfo.id,
+    ];
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+
+      return res.status(200).json("Post has been created");
     });
   });
 };
