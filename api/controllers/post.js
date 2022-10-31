94fce61df286b5b5db7dc208717a5d7645bb711d
Murat Altun <mraltun@gmail.com>
2022-10-31 12:08:27 +0300

    Added jwt token and only show posts from user and user's liked users functionality

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,11 +1,19 @@
+import jwt from "jsonwebtoken";
 import { db } from "../connect.js";
 
 export const getPosts = (req, res) => {
-  const q = `SELECT p.*, u.id AS usedId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.p.userId)`;
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not logged in!");
 
-  db.query(q, (err, data) => {
-    if (err) return res.status(500).json(err);
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
 
-    return res.statu(200).json(data);
+    const q = `SELECT p.*, u.id AS usedId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) LEFT JOIN relationships AS r ON (p.userId = r.followedUserId) WHERE r.followerUserId = ? OR p.userId = ? ORDER BY p.createdAt DESC`;
+
+    db.query(q, [userInfo.id, userInfo.id], (err, data) => {
+      if (err) return res.status(500).json(err);
+
+      return res.status(200).json(data);
+    });
   });
 };
