89dc9c46724ce21b36039c9d9b939752caf2de7e
Murat Altun <mraltun@gmail.com>
2022-11-01 13:53:49 +0300

    Added getUser

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,1 +1,15 @@
-export const getUser = (req, res) => {};
+import { db } from "../connect.js";
+import jwt from "jsonwebtoken";
+
+export const getUser = (req, res) => {
+  const userId = req.params.userId;
+  const q = "SELET * FROM users WHERE id=?";
+
+  db.query(q, [userId], (err, data) => {
+    if (err) return res.status(500).json(err);
+
+    const { password, ...info } = data[0];
+
+    return res.json(info);
+  });
+};
